6c937089230a81cebaeaaae640a0dce7b05f33eb
Ariel Cercado <ariel.cercado@gmail.com>
2018-11-02 00:25:22 +0800

    basic functions works!

---
+++ README.md
@@ -0,0 +1,56 @@
+# Car Finder / Viewer
+
+Search vehicle by ID and pull available information and an image of it
+
+## Introduction
+
+Backend uses python pandas for the dataframe/database and hug.rest for a lightweight REST API server
+Frontend uses vue as the framework with axios for the easy REST API calls and bulma css for the layout
+
+Frontend is completely decoupled with the backend development, only the necessary endpoints are
+needed by the server.
+
+### Prerequisites
+
+First, install the pip requirements/virtualenvironment found in the project root folder
+
+```
+$ pip install -r requirements.txt
+```
+
+### Sample runs
+
+First run the API
+
+```
+$ hug -f src/backend.api.py
+```
+
+Then open the frontend page, works even if opened as it is or hosted in a www/htdocs folder
+
+```
+file:///home/goliath/projects/vehiclefinder/src/frontend/index.html
+```
+
+## Deployment
+
+To use a WSGI-compatible server, we can use uWSGI
+
+```
+$ uwsgi --http 0.0.0.0:8000 --wsgi-file src/backend/api.py --callable __hug_wsgi__
+```
+
+## Built With
+
+* [Python pandas](https://pandas.pydata.org/)
+* [VueJS](https://vuejs.org/)
+* [Axios](https://github.com/axios/axios)
+* [hug REST](http://www.hug.rest/)
+
+## Authors
+
+* **Ariel D. Cercado** - *Initial work*
+
+## License
+
+For use with Chrysalis Solmotive (tech exam)
--- src/backend/api.py
+++ src/backend/api.py
@@ -1,19 +1,24 @@
 import pandas as pd
 import json
-
+import hug
 
 THE_FILE = "./vehicles.csv"
 
-if __name__ == "__main__":
-    # open the csv with pandas
-    csv_obj = pd.read_csv(THE_FILE, sep=",")
+def cors_support(response, *args, **kwargs):
+    response.set_header('Access-Control-Allow-Origin', '*')
+
+@hug.cli()
+@hug.local()
+@hug.get("/get_vehicle", examples="info=12345", requires=cors_support)
+def get_vehicle(info: hug.types.number):
+    csv_obj = pd.read_csv(THE_FILE, sep=",", index_col=False)
+
 
     # test filter/search string
-    SEARCH_QUERY = 'mercedes'
-    FILTER_VEHICLE_MAKE = csv_obj['Make'].str.contains(SEARCH_QUERY.upper())
+    FILTER_VEHICLE_MAKE = csv_obj['Vehicle ID'] == info
 
     # test 5 rows first
-    filter_output = csv_obj[FILTER_VEHICLE_MAKE].head()
+    filter_output = csv_obj[FILTER_VEHICLE_MAKE]
 
     # print out dataframe test, run with $ python api.py
     with pd.option_context('display.max_rows', None, 'display.max_columns', None):
@@ -21,20 +26,6 @@ if __name__ == "__main__":
 
     # convert to json, test print
     jsoned = filter_output.to_json(orient='index')
-    print(jsoned)
-
     json_data = json.loads(jsoned)
-    # print(json_data['1'])
-
-    # iterate json data
-    for item in json_data:
-        ret_id = json_data[item]['Vehicle ID']
-        ret_make = json_data[item]['Make']
-        ret_model_s = json_data[item]['Short Model']
-        ret_model_l = json_data[item]['Long Model']
-        ret_trim = json_data[item]['Trim']
-        ret_derivative = json_data[item]['Derivative']
-        ret_year = json_data[item]['Year introduced']
-        ret_discontinued = json_data[item]['Year discontinued']
-        print("ID: {} Derivative: {}".format(ret_id, ret_derivative))
-
+    print(json_data)
+    return {'data': json_data}
